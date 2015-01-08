cb95d3a54380943cfa5bdf85059af57031eb0e75
antevertonlima <antevertonlima@gmail.com>
2015-01-08 08:46:25 -0300

    correção bandeira finalizada

--- application/views/site/home/home.php
+++ application/views/site/home/home.php
@@ -5,7 +5,7 @@
 
     <div class="col-md-5 col-xs-5 col-sm-5 col-lg-5">
         <img class="foto" src="<?php echo base_url(); ?>assets/theme_site/img/mi.jpg">
-        <span class="bandeira">19<hr class="dividiBandeira"/>NOV</span>
+        <span class="bandeira"><em class="numero">19</em><hr class="dividiBandeira"/>NOV</span>
     </div>
     
     <div class="col-md-7 col-xs-7 col-sm-7 col-lg-7">
