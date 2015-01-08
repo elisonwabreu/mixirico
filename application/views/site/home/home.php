c7974d77cd5e0aad77af2b6545b9a7e16a14431e
antevertonlima <antevertonlima@gmail.com>
2015-01-08 08:11:29 -0300

    Não sei onde eu mexi

--- application/views/site/home/home.php
+++ application/views/site/home/home.php
@@ -18,35 +18,3 @@
     </div>
 
 </div>
-
-<div class="col-md-12 col-xs-12 col-sm-12 col-lg-12 noticia">
-    <div class="col-md-5 col-xs-5 col-sm-5 col-lg-5">
-        <img class="foto" src="<?php echo base_url(); ?>assets/theme_site/img/mi.jpg">
-        <img class="bandeira" src="<?php echo base_url(); ?>assets/theme_site/img/bandeira.png">
-    </div>
-    <div class="col-md-7 col-xs-7 col-sm-7 col-lg-7">
-        <p class="titulo">Mixirico ganha prêmio de show de humor</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-            placerat diam quis nisl vestibulum dignissim. In hac habitasse
-            platea dictumst. Interdum et malesuada fames ac ante ipsum primis
-            in faucibus. Pellentesque habitant morbi tristique senectus et
-            netus et malesuada fames ac turpis egestas.</p>
-        <a href="#">Clique para ler a notícia completa</a>
-    </div>
-</div>
-
-<div class="col-md-12 col-xs-12 col-sm-12 col-lg-12 noticia">
-    <div class="col-md-5 col-xs-5 col-sm-5 col-lg-5">
-        <img class="foto" src="<?php echo base_url(); ?>assets/theme_site/img/mi.jpg">
-        <img class="bandeira" src="<?php echo base_url(); ?>assets/theme_site/img/bandeira.png">
-    </div>
-    <div class="col-md-7 col-xs-7 col-sm-7 col-lg-7">
-        <p class="titulo">Mixirico ganha prêmio de show de humor</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-            placerat diam quis nisl vestibulum dignissim. In hac habitasse
-            platea dictumst. Interdum et malesuada fames ac ante ipsum primis
-            in faucibus. Pellentesque habitant morbi tristique senectus et
-            netus et malesuada fames ac turpis egestas.</p>
-        <a href="#">Clique para ler a notícia completa</a>
-    </div>
-</div>
