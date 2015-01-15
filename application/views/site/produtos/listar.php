2ad30dc0cd13858473eeb47724927413296ec639
antevertonlima <antevertonlima@gmail.com>
2015-01-15 16:39:05 -0300

    listagem de produtos
    finalizada! ainda falta a pagina de detalhes do produto

--- application/views/site/produtos/listar.php
+++ application/views/site/produtos/listar.php
@@ -1,20 +1,48 @@
 <?php if ( ! defined("BASEPATH")) exit("No direct script access allowed");
 
 if($this->listProdutos != null){
-echo '<ul class="listaprodutos">';
+echo '<div class="col-md-12 col-xs-12 col-sm-12 col-lg-12 listaVideos">';
+echo '<div id="carousel-example" class="carousel slide hidden-xs" data-ride="carousel">';
 foreach ($this->listProdutos as $prods) {
         echo '
-                <li>
-                <div class="thumbS">
-                        '.thumb($prods->arquivo, 124, 124, FALSE, $prods->titulo).'
-                    </div>   
-                    <a href="'.base_url("site/detalhes/$prods->slug").'" class="btn btn-inverse btn-block" title="'.$prods->titulo.'">'.$prods->titulo.'</a>
-                </li>
+                <!-- Wrapper for slides -->
+		<div class="carousel-inner">
+                    <div class="item active">
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <div class="col-item">
+                                    <div class="photo">
+                                            <img src="'.thumb($prods->arquivo, 350, 260, '',FALSE).'" class="img-responsive" alt="'.$prods->titulo.'" />
+                                    </div>
+                                    <div class="info">
+                                        <div class="row">
+                                                <div class="price col-md-6">
+                                                        <h5>'.$prods->titulo.'</h5>
+                                                        <h5 class="price-text-color">R$ '.$prods->preco.'</h5>
+                                                </div>
+                                                <div class="rating hidden-sm col-md-6"></div>
+                                        </div>
+                                        <div class="separator clear-left">
+                                                <p class="btn-add"></p>
+                                                <p class="btn-details">
+                                                    <a href="'.base_url("site/detalhes/$prods->slug").'" class="hidden-sm">Detalhes</a>
+                                                </p>
+                                        </div>
+                                        <div class="clearfix"></div>
+                                </div>
+                        </div>
+                </div>
+                </div>
+                </div>
+                </div>
         ';
 }
-echo '</ul>';
+
+		
+echo '</div>';
+echo '</div>';
 echo '<div class="clearfix"></div>';
 echo "<div class='pagination pagination-large pagination-centered'>" . $this->paginar . "</div>";
 } else {
-    echo '<div class="alert alert-error">Nenhum produto encontrado para essa categoria. Por favor volte mais tarde!</div>';
+    echo '<div class="alert alert-danger" role="alert">Nenhum produto encontrado para essa categoria. Por favor volte mais tarde!</div>';
 }
